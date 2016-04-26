8efa836a94784522360149b18b233f10a7f09f92
afp2 <afp2@njit.edu>
2016-04-25 22:09:28 -0400

    pushing third form

---
+++ 3rd_form.sql
@@ -0,0 +1,126 @@
+-- MySQL dump 10.13  Distrib 5.5.47, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: midterm_project
+-- ------------------------------------------------------
+-- Server version	5.5.47-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `afp2`
+--
+
+DROP TABLE IF EXISTS `afp2`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `afp2` (
+  `gameId` int(11) NOT NULL,
+  PRIMARY KEY (`gameId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `afp2`
+--
+
+LOCK TABLES `afp2` WRITE;
+/*!40000 ALTER TABLE `afp2` DISABLE KEYS */;
+INSERT INTO `afp2` VALUES (1),(2),(5),(7);
+/*!40000 ALTER TABLE `afp2` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bobert`
+--
+
+DROP TABLE IF EXISTS `bobert`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bobert` (
+  `gameId` int(11) NOT NULL,
+  PRIMARY KEY (`gameId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bobert`
+--
+
+LOCK TABLES `bobert` WRITE;
+/*!40000 ALTER TABLE `bobert` DISABLE KEYS */;
+INSERT INTO `bobert` VALUES (2);
+/*!40000 ALTER TABLE `bobert` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `games`
+--
+
+DROP TABLE IF EXISTS `games`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `games` (
+  `gameId` int(11) NOT NULL AUTO_INCREMENT,
+  `gamename` varchar(32) DEFAULT NULL,
+  `releasedate` date DEFAULT NULL,
+  `status` varchar(16) DEFAULT NULL,
+  PRIMARY KEY (`gameId`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `games`
+--
+
+LOCK TABLES `games` WRITE;
+/*!40000 ALTER TABLE `games` DISABLE KEYS */;
+INSERT INTO `games` VALUES (1,'Subpar_Mario_Bros.','2016-08-13','NORMAL'),(2,'Subpar_Mario_Bros._2','2016-08-14','NORMAL'),(3,'Half-Life_3','3001-07-07','CANCELLED'),(4,'Codfish_of_Duty','2016-05-06','NORMAL'),(5,'Paint_Drying_Simulator','2017-01-15','CANCELLED'),(6,'Poker_Mon','2016-10-21','NORMAL'),(7,'Portal_8','2020-08-08','NORMAL');
+/*!40000 ALTER TABLE `games` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `login` (
+  `loginId` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `privilegeLevel` int(11) DEFAULT NULL,
+  `displayName` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`loginId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `login`
+--
+
+LOCK TABLES `login` WRITE;
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+INSERT INTO `login` VALUES (2,'afp2','9c1b0b7fec259edb0f6a',2,NULL),(3,'bobert','24a43c06a85bd980a72a',NULL,NULL);
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-04-25 22:07:48
